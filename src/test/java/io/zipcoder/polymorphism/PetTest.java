2d576783b7ee454c6e9cdf3fa60a2188b91356d0
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-12 21:59:43 -0400

    wrote tests for pet

--- src/test/java/io/zipcoder/polymorphism/PetTest.java
+++ src/test/java/io/zipcoder/polymorphism/PetTest.java
@@ -1,26 +1,45 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PetTest {
 
     @Test
     public void testTestGetName() {
-
+        String expected = "Benny";
+        Pet doggo = new Dog(expected);
+        String actual = doggo.getName();
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testTestSetName() {
-
+        String expected = "Benny";
+        Pet doggo = new Dog(expected);
+        doggo.setName("Louis");
+        String actual = doggo.getName();
+        Assert.assertNotEquals(expected, actual);
+        Assert.assertEquals("Louis", actual);
     }
 
     @Test
     public void testSpeak() {
+        String name = "Benny";
+        Pet doggo = new Dog(name);
+        String expected = "Woof!";
 
+        String actual = doggo.speak();
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testTestToString() {
+        String name = "Benny";
+        Pet doggo = new Dog(name);
+        String actual = doggo.toString();
+        String expected = "\nBenny says Woof!";
 
+        Assert.assertEquals(expected, actual);
     }
 }
